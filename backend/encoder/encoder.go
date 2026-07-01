336ca40413d40e255635e6ef7d5f984ef3b8f9c1
khalidoukoujane <oukoujanekhalid9@gmail.com>
2026-07-01 18:23:20 +0100

    added encoder

---
+++ backend/encoder/encoder.go
@@ -0,0 +1,20 @@
+package encoder
+
+func EncodeFrame(raw []byte, width, height int) []byte {
+	chars := []byte("@#S%?*+;:,. ")
+	elems := make([]byte, 0, width*height*4)
+	for i := 0; i < len(raw); i+=3 {
+		r := raw[i]
+		g := raw[i + 1]
+		b := raw[i + 2]
+		brightness := (int(r) + int(g) + int(b)) / 3
+		charIndex := brightness * (len(chars)-1) / 255
+		char := chars[charIndex]
+		outIndex := (i / 3) * 4
+		elems[outIndex] = r
+		elems[outIndex + 1] = g
+		elems[outIndex + 2] = b
+		elems[outIndex + 3] = char
+	}
+	return  elems;
+}
