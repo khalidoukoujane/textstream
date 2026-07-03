bcb1cb61f3a4b101013e8fc5efbe40d97add0a45
khalidoukoujane <oukoujanekhalid9@gmail.com>
2026-07-03 22:09:00 +0100

    added args for file path

--- backend/main.go
+++ backend/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/khalidoukoujane/textstream/server"
 )
 
 func main() {
+	args := os.Args[1:]
 	fmt.Println("Hello")
-	server.StartServer()
+	server.StartServer(args[0])
 }
