13bd9dc11c6f2265d7307c8464b2559e3327bae5
khalidoukoujane <oukoujanekhalid9@gmail.com>
2026-07-03 22:08:38 +0100

    server almost done

--- backend/server/server.go
+++ backend/server/server.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/khalidoukoujane/textstream/decoder"
+	"github.com/khalidoukoujane/textstream/encoder"
 )
+
+var vidPath string
 
 var upgrader = websocket.Upgrader {
 	CheckOrigin: func(r *http.Request) bool {
@@ -15,11 +20,28 @@ var upgrader = websocket.Upgrader {
 }
 
 func wsHandler (w http.ResponseWriter, r *http.Request) {
-	// conn, err:= upgrader.Upgrade(w, r, nil)
-
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+	frames := make(chan []byte, 10)
+	ticker := time.NewTicker(time.Second / 30)
+	go decoder.StartDecoder(vidPath, frames)
+	for frame := range(frames) {
+		data := encoder.EncodeFrame(frame, 160, 90)
+		<-ticker.C
+		err := conn.WriteMessage(websocket.BinaryMessage, data)
+		if err != nil {
+			log.Println("client disconnected")
+			break
+		}
+	}
 }
 
-func StartServer() {
+func StartServer(videoPath string) {
+	vidPath = videoPath
 	http.HandleFunc("/ws", wsHandler)
 	frontendPath := "../frontend/dist"
 	http.Handle("/", http.FileServer(http.Dir(frontendPath)))
